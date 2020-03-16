11b11bc14a1a91eab56741f3805caf1e52bb9416
claudia-maria-dudau <62221313+claudia-maria-dudau@users.noreply.github.com>
2020-03-16 20:35:09 +0200

    constructor fara parametrii

--- Node.cpp
+++ Node.cpp
@@ -5,3 +5,9 @@ Node::Node() { //constructor fara parametrii
 	this->info = this->pr = 0;
 	this->next = nullptr;
 }
+
+Node::Node(int x, int p, Node* n) { //constructor fara parametrii
+	this->info = x;
+	this->pr = p;
+	this->next = n;
+}
