4c5489f19899a106f9429b64bdb4ba562bdcd5d8
claudia-maria-dudau <62221313+claudia-maria-dudau@users.noreply.github.com>
2020-03-16 20:31:35 +0200

    header Node

--- Node.h
+++ Node.h
@@ -1,4 +1,10 @@
 #pragma once
 class Node {
-};
+	int info, pr;
+	Node* next;
 
+public:
+	Node(); //consrtuctor fara param
+	Node(int, int, Node* n); //constructor ce creeaza un nod cu val si prioritatea data
+	Node(Node& n); //constructor de copiere
+};
