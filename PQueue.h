eb5a76f5908c0c0ffd341e1f65f60668cd813fe2
claudia-maria-dudau <62221313+claudia-maria-dudau@users.noreply.github.com>
2020-03-16 22:29:49 +0200

    header PQueue

--- PQueue.h
+++ PQueue.h
@@ -1,4 +1,26 @@
 #pragma once
-class PQueue {
-};
+#include <iostream>
+#include "Node.h"
+#include "Node.cpp"
 
+class PQueue {
+	int size;
+	Node* start;
+
+public:
+	PQueue(); //constructor fara parametrii
+	PQueue(int []); //constructor cu parametrii
+	PQueue(PQueue& pq); //constructor de copiere
+	~PQueue(); //destrcutor
+	void push(); //adaugare element
+	void pop(); //eliminare element
+	int getSize(); //obtinere numar elemente
+	int getMax(); //obtinere maxim
+	int getPrMax(); //obtinere prioritatea maxima
+	void operator =(PQueue&); //supraincarcare operator =
+	PQueue operator +(PQueue&); //supraincarcare operator +
+	void operator ++(); //supraincarcare operator ++
+	void operator --(); //supraincarcare operator --
+	friend ostream& operator <<(ostream&, PQueue&); //supraincarcare operator << (afisare)
+	friend istream& operator >>(istream&, PQueue&); //supraincarcarea operatorului >> (citire)
+};
