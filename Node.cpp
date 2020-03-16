a0ee849d83cca18d9ab9ebceccc73abe1a870805
claudia-maria-dudau <62221313+claudia-maria-dudau@users.noreply.github.com>
2020-03-16 22:58:01 +0200

    no constructor fara parametrii

--- Node.cpp
+++ Node.cpp
@@ -1,11 +1,6 @@
 #include "Node.h"
 using namespace std;
 
-Node::Node() { //constructor fara parametrii
-	this->info = this->pr = 0;
-	this->next = nullptr;
-}
-
 Node::Node(int x, int p, Node* n) { //constructor fara parametrii
 	this->info = x;
 	this->pr = p;
--- Node.h
+++ Node.h
@@ -4,7 +4,6 @@ class Node {
 	Node* next;
 
 public:
-	Node(); //consrtuctor fara param
 	Node(int, int, Node* n); //constructor ce creeaza un nod cu val si prioritatea data
 	Node(Node& n); //constructor de copiere
 	friend class PQueue;
